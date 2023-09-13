e8ac9f864d5de90fb650badb165bd7c3405dd5dc
TheRealWagon <thomas.odvart@gmail.com>
2023-09-13 16:12:47 +0200

    added gitignore

--- firnenergy/src/components/body/admin/adminPage.jsx
+++ firnenergy/src/components/body/admin/adminPage.jsx
@@ -1,9 +1,12 @@
+import { useEffect } from 'react';
 import UsersList from './users/usersList'
 
 
 const AdminPage = () => {
 
-
+useEffect(() => {
+    console.log("this runs")
+})
     
     return (
         <>
